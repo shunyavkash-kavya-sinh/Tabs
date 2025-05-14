97cad001bf3c8eff57e61fb71f76f4f1cd27359c
Sinh Kavya <kavya.singh@shunyavkash.com>
2025-05-14 15:30:15 +0530

    Tabs

---
+++ assests/js/main.js
@@ -0,0 +1,22 @@
+function showSection(id) {
+  const sections = document.querySelectorAll(".tabs-dec");
+  sections.forEach((sec) => sec.classList.remove("active-tab"));
+  document.getElementById(id).classList.add("active-tab");
+
+  const tabButtons = document.querySelectorAll(".tabs-btn ul li");
+  tabButtons.forEach((btn) => btn.classList.remove("active-btn"));
+
+  tabButtons.forEach((btn) => {
+    if (btn.innerText.trim() === id) {
+      btn.classList.add("active-btn");
+    }
+  });
+}
+
+function setDarkMode() {
+  document.body.classList.add("dark-mode");
+}
+
+function setLightMode() {
+  document.body.classList.remove("dark-mode");
+}
